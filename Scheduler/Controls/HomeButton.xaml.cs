ff50ffda83d97c06631b8acc063db83b4e80111e
Jonika <katyadyduch@gmail.com>
2022-12-09 13:21:39 +0200

    upd

---
+++ Scheduler/Controls/HomeButton.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scheduler.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для HomeButton.xaml
+    /// </summary>
+    public partial class HomeButton : UserControl
+    {
+        public HomeButton()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Line.Visibility = Visibility.Visible;
+           
+        }
+
+        private void RadioBtn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Line.Visibility = Visibility.Hidden;
+        }
+    }
+}
